7fa33d0a7dede738fd039c1bb9d05ed1bf5d2578
Ravi Teja Gudapati <tejainece@gmail.com>
2025-02-21 23:48:13 +0100

    Eval ellipse

---
+++ example/eval_ellipse.dart
@@ -0,0 +1,16 @@
+import 'package:equation/equation.dart';
+
+void main() {
+  Eq ellipse =
+      ((x - h) * Cos(theta) + (y - k) * Sin(theta)).pow(Constant(2)) /
+          r1.pow(Constant(2)) +
+      ((x - h) * Sin(theta) - (y - k) * Cos(theta)).pow(Constant(2)) /
+          r2.pow(Constant(2)) -
+      one;
+  ellipse = ellipse.simplify();
+  print(ellipse);
+
+  final quad = ellipse.asQuadratic(x);
+  print(quad);
+  print(quad.solve());
+}
